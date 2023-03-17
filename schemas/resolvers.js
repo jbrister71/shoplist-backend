5a7558123955b86aaea35e7437514ddc2ad1722d
Bell <jacobbrister71@gmail.com>
2023-03-17 17:10:10 -0400

    updated resolvers to properly save and load user data

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -7,5 +7,14 @@ const resolvers = {
             return User.find()
                 .select('-__v -password');
         }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            
+            return { user };
+        }
     }
 }
+
+module.exports = resolvers;
