244b5c4c56b10421320c26bed53d7c92c61639b7
Bell <jacobbrister71@gmail.com>
2023-03-20 13:56:50 -0400

    added ShopList typedefs

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -2,14 +2,26 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
+    type ShopList {
+        _id: ID
+        username: String
+        name: String
+        Price: Float
+        Store: String
+        Persist: Boolean
+        Bought: Boolean
+    }
+
     type User {
         _id: ID
         username: String
         email: String
+        shopList: [ShopList]
     }
 
     type Query {
         users: [User]
+        shopList(username: String!): [shopList]
     }
 
     type Mutation {
